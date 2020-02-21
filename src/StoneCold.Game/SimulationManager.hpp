41c55a2973538432b8d5525b55b4677265cf6df8
Peter Baumann <peterbaumann2@gmx.de>
2020-02-21 07:53:17 +0100

    Updated SimulationManager comments

--- src/StoneCold.Game/SimulationManager.hpp
+++ src/StoneCold.Game/SimulationManager.hpp
@@ -14,9 +14,15 @@ using namespace StoneCold::Engine;
 using namespace StoneCold::Resources;
 
 //
-// Definitions of what Resources should be loaded
-// This includes Player Textures, GUI Fonts, NPC Animations, ...
+// SimulationManager
 //
+// One of the StoneCold::Game centrepieces, the SimulationManager
+// triggers Resource loading and unloading, creates GameObjects 
+// with those Resources and adds/removes them to/from the Engine.
+//
+// Creating the wohle simulation includes: Loading the GUI and PlayerCharacter, 
+// generating Maps, creating Levels from those Maps by adding Tiles and NPCs, etc.
+// 
 class SimulationManager {
 public:
 	SimulationManager() : _engine(nullptr), _resourceManager(nullptr) { }
@@ -26,20 +32,20 @@ public:
 	bool Initialize(EngineCore* engine, ResourceManager* resourceManager);
 
 	//
-	// ...
+	// Clear and (Re-)Load all Global Resources (PlayerCharacter, Fonts, etc.),
+	// create the specific GameObjects and feed them to the Engine if needed.
 	//
 	void LoadGlobalResouces();
 
 	//
-	// Load a Level (Map)
-	//
-	// Create a Map by loading all needed Textures based on LevelType (Grass, Desert, ...)
-	// Then those Textures are transformed into MapTiles, based on the MapLayout and finally 
-	// pushed to the Engines GameObject List, which will be rendered
+	// Clear and (Re-)Load all Level Resources (Map Textures, NPC Textures/Animations), create
+	// a new procedurally generated Map and populate that Map with its Tiles and Mobs, etc.
+	// Finally add those GameObjects to the Engine, if needed.
 	//
 	void LoadLevelResouces();
 
 	//
+	// Clear and (Re-)Load all Sequence Resources (Cutscene Textures, etc.)
 	// ...
 	//
 	void LoadSequenceResouces();
